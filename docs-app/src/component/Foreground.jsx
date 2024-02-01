f08458add90ff13cf7fa4d6ecbbdbf36c50f9d4f
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-01 22:57:01 +0530

    Showing multiple cards

--- docs-app/src/component/Foreground.jsx
+++ docs-app/src/component/Foreground.jsx
@@ -8,13 +8,27 @@ const Foreground = () => {
             filesize: ".9mb",
             close: true,
             tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
         }
     ];
 
     return (
         <>
-            <div className='fixed top-0 left-0 z-[3] w-full h-full'>
-                <Card />
+            <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+                {data.map((item, index) => (
+                    <Card data={item} />
+                ))}
             </div>
         </>
     )
