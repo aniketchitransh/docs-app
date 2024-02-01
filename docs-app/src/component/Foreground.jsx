939fcb46ae081db5e0045618ec1c764c4b0969b6
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-01 22:41:21 +0530

    Created a dummy data

--- docs-app/src/component/Foreground.jsx
+++ docs-app/src/component/Foreground.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+    const data = [
+        {
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+        }
+    ];
+
     return (
         <>
             <div className='fixed top-0 left-0 z-[3] w-full h-full'>
