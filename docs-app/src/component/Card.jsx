74ba2fc646c07d951a7be3b576bcc651691dc809
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-01 22:19:17 +0530

    Designing card

--- docs-app/src/component/Card.jsx
+++ docs-app/src/component/Card.jsx
@@ -4,11 +4,12 @@ import { FaRegFileAlt } from "react-icons/fa";
 
 const Card = () => {
     return (
-        <div className='w-60 h-72 rounded-[20px] bg-zinc-900/90 text-white p-5'>
+        <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
             <FaRegFileAlt />
-            <p className=''>
+            <p className='text-sm leading-tight mt-5 font-semibold'>
                 Lorem ipsum dolor sit amet, consectetur adipisicing.
             </p>
+            <div className='footer absolute bottom-0 w-full h-20 left-0 bg-sky-200'></div>
         </div>
     )
 }
