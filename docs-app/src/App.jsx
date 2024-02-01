238460cfe47897f80db5c2f18ba7f9f7875292e7
Aniket Chitransh <ani.chitransh@gmail.com>
2024-02-01 21:52:06 +0530

    Created a component Background

--- docs-app/src/App.jsx
+++ docs-app/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Background from './component/Background'
 
 const App = () => {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900 text-[12vw] leading-none tracking-tighter'>Docs</h1>
+      <Background />
     </div>
   )
 }
---
+++ docs-app/src/component/Background.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Background = () => {
+    return (
+        <div>
+            <div className='w-full absolute top-[5%] py-10 flex justify-center text-zinc-600 text-xl font-semibold'>Documents.</div>
+            <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] 
+                font-semibold text-zinc-900 text-[12vw] leading-none tracking-tighter'>
+                Docs.
+            </h1>
+        </div>
+    )
+}
+
+export default Background
